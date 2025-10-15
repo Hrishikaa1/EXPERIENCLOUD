e99d70b3dd9011da5fcc2308ef491b2c7ccffd68
Hrishika Dhakad <126436830+Hrishikaa1@users.noreply.github.com>
2025-10-15 12:04:51 +0530

    Add files via upload

---
+++ pictureback.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import BackImage from '@salesforce/resourceUrl/BackImage';
+ 
+export default class Pictureback extends LightningElement {
+    Img = BackImage;
+}
